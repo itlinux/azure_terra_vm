76f2a22c236e861f283762cd6f1b6fa072753da2
Remo Mattei <remo@mattei.org>
2020-05-11 10:41:23 -0700

    adding TF version

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+terraform {
+  required_version = "> 0.12.0"
+}
+
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
   # If you are using version 1.x, the "features" block is not allowed.
