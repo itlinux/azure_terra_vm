5062277c867e93b0b25f54f0794deaa3dd441319
Remo Mattei <remo@mattei.org>
2020-05-11 11:29:57 -0700

    removing web from this branch

--- web.tf
+++ web.tf
@@ -1,14 +1,14 @@
-resource "azurerm_virtual_machine_extension" "apache" {
-  depends_on           = [azurerm_linux_virtual_machine.it_tf_vm]
-  name                 = "apache"
-  publisher            = "Microsoft.Azure.Extensions"
-  type                 = "CustomScript"
-  type_handler_version = "2.0"
-  count                = var.number_of_vm
-  virtual_machine_id   = azurerm_linux_virtual_machine.it_tf_vm[0].id
-  settings             = <<SETTINGS
-{
-  "commandToExecute": "sudo  bash -c 'yum install -y epel-release && yum install -y nginx &&  systemctl --now enable nginx && echo ${var.web_msg} > /usr/share/nginx/html/index.html'"
-}
-SETTINGS
-}
+# resource "azurerm_virtual_machine_extension" "apache" {
+#   depends_on           = [azurerm_linux_virtual_machine.it_tf_vm]
+#   name                 = "apache"
+#   publisher            = "Microsoft.Azure.Extensions"
+#   type                 = "CustomScript"
+#   type_handler_version = "2.0"
+#   count                = var.number_of_vm
+#   virtual_machine_id   = azurerm_linux_virtual_machine.it_tf_vm[0].id
+#   settings             = <<SETTINGS
+# {
+#   "commandToExecute": "sudo  bash -c 'yum install -y epel-release && yum install -y nginx &&  systemctl --now enable nginx && echo ${var.web_msg} > /usr/share/nginx/html/index.html'"
+# }
+# SETTINGS
+# }
