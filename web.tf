eee1a4f9067c65a4e5c992362e335b12bb15efae
Remo Mattei <remo@mattei.org>
2020-05-31 13:46:53 -0700

    changing vars on web.tf with count.index

--- web.tf
+++ web.tf
@@ -5,7 +5,7 @@ resource "azurerm_virtual_machine_extension" "apache" {
   type                 = "CustomScript"
   type_handler_version = "2.0"
   count                = var.number_of_vm
-  virtual_machine_id   = azurerm_linux_virtual_machine.it_tf_vm[0].id
+  virtual_machine_id   = azurerm_linux_virtual_machine.it_tf_vm[count.index].id
   settings             = <<SETTINGS
  {
   "commandToExecute": "sudo yum install -y httpd &&  systemctl --now enable httpd && echo ${var.web_msg} >/var/www/html/index.html"
